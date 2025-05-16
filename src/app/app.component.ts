e264aa552ea49fe68513b9160e5dcc61bd4437c7
josue <josuex.x.x@hotmail.com>
2025-05-16 11:44:54 -0300

    feat(components): add core application components
    - Generate CanvasComponent for SVG rendering
    - Create ToolbarComponent for shape manipulation
    - Add PropertyPanelComponent for editing properties
    - Setup basic layout structure in app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ToolbarComponent } from "./components/toolbar/toolbar.component";
+import { CanvasComponent } from "./components/canvas/canvas.component";
+import { PropertyPanelComponent } from "./components/property-panel/property-panel.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ToolbarComponent, CanvasComponent, PropertyPanelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/property-panel/property-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-panel',
+  imports: [],
+  templateUrl: './property-panel.component.html',
+  styleUrl: './property-panel.component.scss'
+})
+export class PropertyPanelComponent {
+
+}
